0be001307a5509b8bcf07ba57c4fac410bbee4b6
rmg-qa <gorohovikroman123@yandex.com>
2025-09-27 01:52:21 +0700

    Выполнение дз № 8. Попытка № 2

--- homework/models.py
+++ homework/models.py
@@ -28,10 +28,9 @@ class Product:
             Проверьте количество продукта используя метод check_quantity
             Если продуктов не хватает, то выбросите исключение ValueError
         """
-        if quantity <= self.quantity:
-            return Product.check_quantity(self, quantity)
-        else:
-            return ValueError
+        if not self.check_quantity(quantity):
+            raise ValueError('Недостаточно товара на складе')
+        self.quantity -= quantity  # уменьшили остаток
 
     def __hash__(self):
         return hash(self.name + self.description)
@@ -69,17 +68,15 @@ class Cart:
         Если remove_count не передан, то удаляется вся позиция
         Если remove_count больше, чем количество продуктов в позиции, то удаляется вся позиция
         """
-        if remove_count is None:
-            Cart.clear(self)
-        elif remove_count > self.products[product]:
+        if product not in self.products:
+            return  # продукта нет, ничего не делаем
+        if remove_count is None or remove_count >= self.products[product]:
             del self.products[product]
         else:
-            new_key_value = self.products[product] - remove_count
-            self.products[product] = new_key_value
-        return self.products
+            self.products[product] -= remove_count
 
     def clear(self):
-        return self.products.clear()
+        self.products.clear()
 
     def get_total_price(self) -> float:
         total_price = 0
@@ -93,10 +90,6 @@ class Cart:
         Учтите, что товаров может не хватать на складе.
         В этом случае нужно выбросить исключение ValueError
         """
-        total_quantity = 0
-        for product in self.products.keys():
-            total_quantity += product.quantity
-        if total_quantity >= 1000:
-            return Cart.clear(self)  # Я не понимаю, что должен делать метод покупки. Надоело гадать в этом дз: самое логичное, что приходит в голову это очистка корзины
-        else:
-            return ValueError
+        for product, quantity in self.products.items():
+            product.buy(quantity)
+        self.clear()
